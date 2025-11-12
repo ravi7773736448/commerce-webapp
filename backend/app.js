6bbffb58ca219af5bf9e945562b05aa3b6dd235c
Ravi Sharma <rs5957636@gmail.com>
2025-11-12 12:12:05 +0530

    sfsdfs

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors("https://commerce-nine-beige.vercel.app/"));
 
 // Connect to MongoDB
 mongoose
