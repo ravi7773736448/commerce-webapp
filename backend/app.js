96889aa038fd72d30eda9dad0ba629e547028199
Ravi Sharma <139843124+ravi7773736448@users.noreply.github.com>
2025-11-22 18:18:58 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors("https://commerce-nine-beige.vercel.app/"));
+app.use(cors("https://whizrobo-uvjw.vercel.app/"));
 
 // Connect to MongoDB
 mongoose
